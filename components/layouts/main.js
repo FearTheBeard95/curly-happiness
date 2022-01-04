910dec4d98532bb49179e2f7c6200526625b5ed3
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-01-04 21:15:57 +0200

    Edit Metadata

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -8,18 +8,16 @@ const Main = ({ children, router }) => {
     <Box as='main' pb={8}>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='description' content="Takuya's homepage" />
-        <meta name='author' content='Takuya Matsuyama' />
-        <meta name='author' content='craftzdog' />
+        <meta name='description' content="Carlos's homepage" />
+        <meta name='author' content='Carlos Sibalatani' />
+        <meta name='author' content='Carlos Sibalatani' />
         <link rel='apple-touch-icon' href='apple-touch-icon.png' />
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@craftzdog' />
-        <meta name='twitter:creator' content='@craftzdog' />
-        <meta name='twitter:image' content='/card.png' />
-        <meta property='og:site_name' content="Takuya Matsuyama's Homepage" />
+        <meta name='twitter:image' content='/images/profile.jpeg' />
+        <meta property='og:site_name' content="Carlos's Homepage" />
         <meta property='og:type' content='website' />
-        <meta property='og:image' content='/card.png' />
+        <meta property='og:image' content='/images/profile.jpeg' />
         <title>Carlos Sibalatani - Homepage</title>
       </Head>
 
